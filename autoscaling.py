4eea5ee02034714f3e4c3c373394074eda0c0e01
jumping <quzhengping@gmail.com>
2013-03-21 16:23:28 +0800

    simple the call logical

--- autoscaling.py
+++ autoscaling.py
@@ -7,44 +7,44 @@ import boto.ec2.elb.loadbalancer
 
 DEBUG = False
 #DEBUG = True
-connec2 = boto.connect_ec2()
-instance = boto.ec2.instance.Instance(connec2)
+#connec2 = boto.connect_ec2()
+#instance = boto.ec2.instance.Instance(connec2)
 
-def instance_info(id):
-    instance.id = id
-    try:
-        instance.update()
-        ip_address =  instance.ip_address
-        private_ip = instance.private_ip_address
-        if DEBUG: print ip_address,private_ip
-        return ip_address,private_ip
-    except:
-        return
+#def instance_info(id):
+#    instance.id = id
+#    try:
+#        instance.update()
+#        ip_address =  instance.ip_address
+#        private_ip = instance.private_ip_address
+#        if DEBUG: print ip_address,private_ip
+#        return ip_address,private_ip
+#    except:
+#        return
 
-def activities_filter(actives):
-    lines = filter(None, actives)
-    instances = {}
-    for line in lines:
-        print line
-        #
-        #print detail messages:
-        #                     line.cause
-        #
-        splited = str(line).split(':')
-        #if splited[0] == 'Activity' and splited[3] == 'Successful progress:100':
-        if splited[0] == 'Activity':
-            #get instance id
-            stat = splited[1].split()
-            (id, flag) = (stat[3], stat[0])
-            if id == 'EC2':
-                id = splited[2].split()[0]
-            if instances.has_key(id):
-                instances[id].append(flag)
-            else:
-                instances[id] = [flag]
-        else:
-            print "Could recognized : %s" %line
-    return instances
+#def activities_filter(actives):
+#    lines = filter(None, actives)
+#    instances = {}
+#    for line in lines:
+#        print line
+#        #
+#        #print detail messages:
+#        #                     line.cause
+#        #
+#        splited = str(line).split(':')
+#        #if splited[0] == 'Activity' and splited[3] == 'Successful progress:100':
+#        if splited[0] == 'Activity':
+#            #get instance id
+#            stat = splited[1].split()
+#            (id, flag) = (stat[3], stat[0])
+#            if id == 'EC2':
+#                id = splited[2].split()[0]
+#            if instances.has_key(id):
+#                instances[id].append(flag)
+#            else:
+#                instances[id] = [flag]
+#        else:
+#            print "Could recognized : %s" %line
+#    return instances
 
 #def running_ip(autoscalingname=''):
 #    history = []
@@ -72,12 +72,12 @@ def activities_filter(actives):
 #                ips.append(r[-1][0])
 #    return ips
 
-def running_ip(autoscalingname=''):
-    history = []
-    running = []
-    ips = []
+def running_ip(autoscalingname='',public=True):
+    #history = []
+    #running = []
+    #ips = []
     
-    conn = boto.connect_elb()
+    conn_elb = boto.connect_elb()
     elbname = '-'.join(autoscalingname.split('-')[0:2])
     #conn = boto.connect_autoscale()
     #groups = conn.get_all_groups()
@@ -86,19 +86,29 @@ def running_ip(autoscalingname=''):
     #actives = conn.get_all_activities(autoscalingname)
     #if DEBUG: print actives
 
-    lb = boto.ec2.elb.loadbalancer.LoadBalancer(connection=conn, name=elbname)
+    #lb = boto.ec2.elb.loadbalancer.LoadBalancer(connection=conn, name=elbname)
 
-    instances = lb.get_instance_health()
-    for instance in instances:
-        if instance.state == u'InService':
-            running.append((instance,instance_info(instance.instance_id)))
-    if running: 
-        #serving instance public ips
-        for r in running:
-            if r[-1]:  
-                if DEBUG: print r
-                ips.append(r[-1][0])
-    return ips
+    #instances = lb.get_instance_health()
+    instances = conn_elb.describe_instance_health(elbname)
+    instance_ids = [ a.instance_id for a in instances if a.state == u'InService' ]
+    conn_ec2 = boto.connect_ec2()
+    reservations = conn_ec2.get_all_instances(instance_ids)
+    private_ips = [ r.private_ip_address for res in reservations for r in res.instances ]
+    public_ips = [ r.ip_address for res in reservations for r in res.instances ]
+
+    #for instance in instances:
+    #    if instance.state == u'InService':
+    #        running.append((instance,instance_info(instance.instance_id)))
+    #if running: 
+    #    #serving instance public ips
+    #    for r in running:
+    #        if r[-1]:  
+    #            if DEBUG: print r
+    #            ips.append(r[-1][0])
+    if public:
+        return public_ips
+    else:
+        return private_ips
 
 def main(autoscalingname):
     #history = []
